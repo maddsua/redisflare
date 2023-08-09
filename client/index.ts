de790ada6c219de37bbbc7c9ced480fceada3dfc
maddsua <maddsuadev@proton.me>
2023-08-09 03:27:55 +0300

    Update index.ts

--- client/index.ts
+++ client/index.ts
@@ -1,35 +1,36 @@
+import type { RedisflareResponse, RedisflareRequest } from '../common/apitypes';
+
 export interface AuthData {
 	domain: string;
 	token: string;
+};
+
+export interface ClientReturnValue {
+	success: RedisflareResponse['success'];
+	error?: Error;
+	data?: RedisflareResponse['data'];
 };
 
 type FetchType = (input: RequestInfo | URL, init?: RequestInit) => Promise<Response>;
 let fetcher: FetchType | undefined = undefined;
 export const setFetcher = (_fetcher: FetchType) => fetcher = _fetcher;
 
-export const get = (auth: AuthData, recordID: string) => new Promise<string>(resolve => {
+const noFetchMessage = `On Node, you need to provide a fetch function using setFetcher() before using the client. This is required due to Node devs being slowpokes with implementing proper fetch api. This error will be removed once a LTS version of Node will fully support the fetch api.`;
 
-	if (!fetcher) return {
+export const get = (auth: AuthData, recordID: string) => new Promise<ClientReturnValue>(resolve => {
 
-	};
+	if (!fetcher) {
+		resolve({ success: false, error: new Error(noFetchMessage) });
+		return;
+	}
 
-	fetcher(`https://${auth.domain}/get?record_id=${recordID}&token=${auth.token}`).then(rsp => rsp.json()).then((data) => {
+	const remoteUrl = new URL(`https://${auth.domain}/get`);
+	remoteUrl.searchParams.append('record_id', recordID);
+	remoteUrl.searchParams.append('token', auth.token);
 
-		if (!data.success) throw new Error(`Request failed: ${data.reason}`);
-
-		if (!data.data) {
-			console.warn('No such db record, gonna create it later');
-			resolve(structuredClone(defaultData))
-			return;
-		}
-
-		const remoteMetrics = JSON.parse(data.data);
-		if (typeof remoteMetrics['updated'] !== 'number') throw new Error('Mangled DB record.');
-		resolve(remoteMetrics);
-
-	}).catch(error => {
-		console.error('DB read failed:', error);
-		resolve(null);
-	});
+	fetcher(remoteUrl).then(rsp => rsp.json()).then((data: RedisflareResponse) => {
+		if (!data.success) throw new Error(data.error_text || 'Unknown error');
+		resolve(data);
+	}).catch(error => resolve({ success: false, error: new Error((error as Error)?.message || error) }));
 
 });
